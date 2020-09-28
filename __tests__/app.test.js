a1bee918e13c74fa44bf11e664bb66823c4eae30
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-09-28 15:26:01 -0700

    add user signup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,24 @@ const app = require('../lib/app');
 
 describe('tardygram-lab routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('checks that a new user is signed up using POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'create@user1.com',
+        password: 'user1password',
+        profile_photo_url: 'profile-photo-url.user1'
+      });
+
+    // do not send password or password hash back
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'create@user1.com',
+      profile_photo_url: 'profile-photo-url.user1'
+    });
+
   });
 });
